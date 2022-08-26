b2a1a4e8a9dc6211bb61560fe37adc6c045af62a
Lom-Ali <lomaliyakubov@gmail.com>
2022-08-26 15:28:07 +0300

    Add signatures for action handlers

--- server.py
+++ server.py
@@ -8,10 +8,39 @@ app = initialize_app()
 
 @app.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+    await message.reply(
+        "Бот ораганайзер\n\n"
+        "Добавить задачу: /add\n"
+        "Сегодняшние задачи: /today\n"
+        "По дате: /date"
+    )
+
+
+@app.message_handler(commands=['add'])
+async def add_task(message: types.Message):
+    """Добавляет новую задачу"""
+    # 1 введите задачу
+    # 2 введите дату или выберите сегодняшнюю дату
+    await message.reply()
+
+
+@app.message_handler(commands=['today'])
+async def today_tasks(message: types.Message):
+    """Отправляет задачи на сегодня"""
+    await message.answer()
+
+
+@app.message_handler(commands=['date'])
+async def tasks_by_date(message: types.Message):
+    """Отправляет задачи на конкретную дату"""
+    await message.answer()
+
+
+@app.message_handler(commands=['complete'])
+async def complete_tasks(message: types.Message):
+    """Завершает таски"""
+    await message.answer()
 
 
 if __name__ == '__main__':
