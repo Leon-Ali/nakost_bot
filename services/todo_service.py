ad0d10bf06043560705939f4eccd5c660d4c3975
Lom-Ali <lomaliyakubov@gmail.com>
2022-08-30 22:37:25 +0300

    Refactor UsersRepository and TodoService

--- services/todo_service.py
+++ services/todo_service.py
@@ -1,14 +1,11 @@
 from repositories import UsersRepository
-from settings import TodoServiceConfig
 
 
 class TodoService:
 
-    def __init__(self):
-        self.headers = {'X-API-KEY': TodoServiceConfig.API_TOKEN, 'Content-Type': 'application/json'}
-
-    async def create_user(self, user_id: str) -> None:
-        await UsersRepository.create_user(user_id, self.headers)
+    @staticmethod
+    async def create_user(user_id: str) -> None:
+        await UsersRepository.create_user(user_id)
 
 
 
