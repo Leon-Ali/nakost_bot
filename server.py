72970fef5c31b5de1e2ece3427750538893e339c
Lom-Ali <lomaliyakubov@gmail.com>
2022-09-02 23:23:25 +0300

    Add simple FSMTask state machine

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from aiogram import executor, types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from create_app import initialize_app
 from services import TodoService
@@ -8,10 +11,16 @@ from repositories import UsersRepository
 app = initialize_app()
 
 
+class FSMTask(StatesGroup):
+    task_description = State()
+    date = State()
+
+
 @app.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     """Sends welcome and creates user"""
-    await TodoService.create_user(user_id=message.from_user.id, repo=UsersRepository)
+    user_repo = UsersRepository()
+    await TodoService.create_user(user_id=message.from_user.id, repo=user_repo)
     await message.reply(
         "Organizer bot\n\n"
         "Create task: /add\n"
@@ -23,9 +32,28 @@ async def send_welcome(message: types.Message):
 @app.message_handler(commands=['add'])
 async def add_task(message: types.Message):
     """Adds new task"""
-    # 1 введите задачу
-    # 2 введите дату или выберите сегодняшнюю дату
-    await message.reply()
+    await FSMTask.task_description.set()
+    await message.reply('Введите имя задачи')
+
+
+@app.message_handler(state=FSMTask.task_description)
+async def add_task_description(message: types.Message, state: FSMContext):
+    """Adds task description"""
+    async with state.proxy() as data:
+        data['task_description'] = message.text
+    await FSMTask.next()
+    await message.reply('Выберите дату для задачи')
+
+
+@app.message_handler(state=FSMTask.date)
+async def add_task_date(message: types.Message, state: FSMContext):
+    """Adds task date"""
+    async with state.proxy() as data:
+        data['date'] = message.date
+
+    async with state.proxy() as data:
+        await message.reply(str(data))
+    await state.finish()
 
 
 @app.message_handler(commands=['today'])
