82b74e2cc7b9a0853f619ea1721d7227ddd5ffe3
Lom-Ali <lomaliyakubov@gmail.com>
2022-09-07 23:34:56 +0300

    Change app init and change message.reply to bot.send_message

--- create_app.py
+++ create_app.py
@@ -1,11 +1,13 @@
+from typing import Tuple
+
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from settings import AppConfig
 
 
-def initialize_app() -> Dispatcher:
+def initialize_app() -> Tuple[Dispatcher, Bot]:
     storage = MemoryStorage()
     bot = Bot(token=AppConfig.API_TOKEN)
     dp = Dispatcher(bot, storage=storage)
-    return dp
+    return dp, bot
