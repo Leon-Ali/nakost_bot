1f5475a3846c6e8223b97ece257a94ae3b3b9697
Lom-Ali <lomaliyakubov@gmail.com>
2022-09-10 23:30:26 +0300

    Add logging and error handling

--- repositories/base_repository.py
+++ repositories/base_repository.py
@@ -1,7 +1,12 @@
+import  logging
+
 import aiohttp
 from aiohttp.web_response import json_response
 
 from settings import TodoServiceConfig
+
+
+logger = logging.getLogger(__name__)
 
 
 class BaseRepository:
@@ -10,5 +15,8 @@ class BaseRepository:
     async def request(self, url, params=None, data=None, method='get') -> json_response():
         async with aiohttp.ClientSession(headers=self.headers) as session:
             request_method = getattr(session, method)
-            raw_response = await request_method(url, json=data, params=params)
-            return await raw_response.json()
+            try:
+                raw_response = await request_method(url, json=data, params=params)
+                return await raw_response.json()
+            except Exception as e:
+                logger.error(f'[ERROR] {e}')
